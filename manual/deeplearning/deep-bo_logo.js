26da4c312ca80c0d12c24f2ac3141028bbb44333
berandon <wangbospace@gmail.com>
2017-04-12 16:24:08 +0100

    'public_init'

---
+++ manual/deeplearning/deep-bo_logo.js
@@ -0,0 +1,7 @@
+document.write('\
+<div class="apple">\
+    <div class="top"><span></span></div>\
+    <div class="butt"><span></span></div>\
+    <div class="bite"></div>\
+</div>\
+');
---
+++ manual/home/home.js
@@ -0,0 +1,41 @@
+var rule_ver = [
+"pin;btt;1900;10;;阳台玻璃面，厨房向阳边墙"
+,"pin;btt;1900;140;;一纵"
+,"pin;btt;1900;470;;二纵"
+,"pin;btt;1900;660;;私厕轻墙"
+,"pin;btt;1900;1060;;三纵"
+]
+var rule_hor = [
+"pin;rtl;170;1500;;主卧主墙"
+,"pin;rtl;270;1500;;次卧主墙"
+,"pin;rtl;570;1500;;一横"
+,"pin;rtl;830;1500;;二横"
+,"pin;rtl;1300;1500;;三横"
+,"pin;rtl;1600;1500;;厨房主墙"
+]
+var room_chinese = [
+]
+var room_euro = [
+";;300;130;room_euro/compact_back.round_bed.mordern.jpg;"
+,";;305;130;room_euro/golden-like.round_bed.euro.jpg;"
+,";;300;135;;圆床周围用罗马帘"
+,";;300;270;room_euro/white_france_luxury.dress_desk.euro.jpg;"
+]
+var room_asia = [
+";;770;130;room_asia/wood_stripe.bay_window.new_chinese.jpg;飘窗减少采光面积,但是让室内更统一.但仅限于大飘窗."
+]
+var room_tea = [
+";;650;700;room_tea/yellow_rectangle_bird_tree.light.chinese.jpg;"
+]
+var sallon = [
+";;840;800;sallon/jianyue_raoxian.canteen.chinese.jpg;背景墙简约,大气,可以将一部分木条直线延伸到全屋"
+,""
+,";;840;805;sallon/hehuaqiang.sallon.chinese.jpg;"
+]
+var canteen = [
+";;1300;200;canteen/xielengge.zhongshi.jpg;推门和窗格比较和谐"
+,";;1000;155;canteen/biangui.jianshi.jpg;边柜用于放酒和装饰花瓶,但中间镂空部分需要和桌子尺寸想对应"
+]
+var house=[rule_ver,rule_hor,sallon,canteen,room_chinese,room_euro,room_asia,room_tea]
+var imgDir = "images/"
+var bk=imgDir+"home.jpg"
